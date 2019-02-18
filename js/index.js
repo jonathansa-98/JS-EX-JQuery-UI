fbb25ae031feba84c7a830a50c740553105651b0
jonathansa-98 <daw2jsuarezalvarez@iesjoanramis.org>
2019-02-18 13:46:10 +0100

    con el 3

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     crearEx1();
     crearEx2();
+    crearEx3();
 });
 
 function crearEx1() {
@@ -16,9 +17,60 @@ function crearEx1() {
 }
 
 function crearEx2() {
-    $(".promo").click(calcPromo);
+    $(".promo").click(function (event) {
+        event.preventDefault();
+        calcPromo($(this));
+    });
 }
 
-function calcPromo() {
-    console.log("hi");
+function calcPromo(promo) {
+    // selecciona el texto del precio
+    var preu = promo.closest('div').prev('.preu');
+    // viatge
+    var viatge = preu.closest('.viatge');
+    viatge.addClass("promocio");
+    // calc descuento
+    var preu_viatge = parseInt(viatge.attr("data-preu"));
+    var dte_viatge = parseInt(viatge.attr("data-dte"));
+    var preu_final = Math.ceil(preu_viatge - (preu_viatge * (dte_viatge / 100)));
+    // poner precio con dte
+    preu.after(`<p class="preu_dte">Promoció ${dte_viatge}%: ${preu_final}€ per setmana</p>`);
+    // cambiar boton
+    var fin_promo = $('<a href="#" class="promo_fin btn btn-info btn-sm"><span class="glyphicon glyphicon-erase"'+
+                        'aria-hidden="true"></span> Finalitzar promoció</a>');
+    promo.replaceWith(fin_promo);
+    console.log(preu_final);
+}
+
+function crearEx3() {
+    /*console.log("hias");
+
+    if ($(".panel-body").hasClass(".promo_fin")){
+        $(".promo_fin").click(function (event) {
+            console.log("hi");
+
+            event.preventDefault();
+            revertPromo($(this));
+        });
+    }*/
+}
+
+function revertPromo(promo) {
+    // selecciona el texto del precio
+  //  var preu = promo.closest('div').prev('.preu');
+//    preu.css("color", "#4775f5a6");
+    // viatge
+    /*var viatge = preu.closest('.viatge');
+    viatge.addClass("promocio");
+    // calc descuento
+    var preu_viatge = parseInt(viatge.attr("data-preu"));
+    var dte_viatge = parseInt(viatge.attr("data-dte"));
+    var preu_final = Math.ceil(preu_viatge - (preu_viatge * (dte_viatge / 100)));
+    // poner precio con dte
+    preu.after(`<p class="preu_dte">Promoció ${dte_viatge}%: ${preu_final}€ per setmana</p>`);
+    // cambiar boton
+    var fin_promo = $('<a href="#" class="promo btn btn-info btn-sm"><span class="glyphicon glyphicon-erase"' +
+        'aria-hidden="true"></span> Finalitzar promoció</a>');
+    promo.replaceWith(fin_promo);
+    console.log(preu_final);*/
 }
